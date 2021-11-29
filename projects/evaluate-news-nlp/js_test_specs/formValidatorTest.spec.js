8883e5fc1881df152facc6590d295863e2af829d
bloodlineCode <51842464+bloodlineCode@users.noreply.github.com>
2021-11-29 17:58:30 -0500

    Adding the missing import

--- projects/evaluate-news-nlp/js_test_specs/formValidatorTest.spec.js
+++ projects/evaluate-news-nlp/js_test_specs/formValidatorTest.spec.js
@@ -1,4 +1,4 @@
-
+import { validateInput } from '../src/client/js/formValidator'
 describe('Testing the submit functionality', () => {
   test('Testing the validateInput function', () => {
     expect(validateInput).toBeDefined()
